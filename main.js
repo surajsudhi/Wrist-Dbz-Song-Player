f4e301b621f5ac40ae7e255cdbba2783f37155dc
surajsudhi <suraj.sudhi@gmail.com>
2022-03-06 21:26:45 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,66 @@
+song1="";
+song2="";
+
+leftWristY=0;
+leftWristX=0;
+
+rightWristX=0;
+rightWristY=0;
+
+function preload(){
+  song1 = loadSound("Beerus.mp3");
+  song2 = loadSound("Obito.mp4");
+  
+  
+}
+
+function setup(){
+canvas = createCanvas(600,500);
+canvas.center();
+
+video = createCapture(VIDEO);
+video.hide();
+
+PoseNet = ml5.poseNet(video, modelLoaded);
+PoseNet.on('pose', gotPoses);
+}
+function modelLoaded(){
+console.log("PoseNet is ready");    
+}
+
+function gotPoses(results){
+if(results.length > 0){
+console.log(results);
+ leftWristX = results[0].pose.leftWrist.x;
+ leftWristY = results[0].pose.leftWrist.y; 
+ console.log("LeftWristX = "+leftWristX+" leftWristY = "+leftWristY);
+ 
+ rightWristX = results[0].pose.rightWrist.x;
+ rightWristY = results[0].pose.rightWrist.y;
+ console.log("RightWristX = "+rightWristX+" RightWristY = "+rightWristY);
+}    
+}
+
+function draw(){
+image(video, 0,0,600,500);
+
+fill("red");
+stroke("red")
+}
+
+function play(){
+if(leftWristY > 0){    
+circle(leftWristX,leftWristY,20);    
+song2.stop();
+song1.play();
+document.getElementById("Song_Name").innerHTML = "Beerus"
+}
+
+if(rightWristY > 0){
+circle(rightWristX,rightWristY,20);    
+song1.stop();
+song2.play();  
+document.getElementById("Song_Name").innerHTML = "Obito"  
+}
+}
+  
