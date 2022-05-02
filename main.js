a50d050b34b1a5e4e5c4743a76193c8637fe1b56
surajsudhi <suraj.sudhi@gmail.com>
2022-05-02 14:08:49 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -4,14 +4,15 @@ song2="";
 leftWristY=0;
 leftWristX=0;
 
+scoreRightWrist = 0;
+scoreLeftWrist = 0;
+
 rightWristX=0;
 rightWristY=0;
 
 function preload(){
   song1 = loadSound("Beerus.mp3");
   song2 = loadSound("Obito.mp4");
-  
-  
 }
 
 function setup(){
@@ -31,6 +32,11 @@ console.log("PoseNet is ready");
 function gotPoses(results){
 if(results.length > 0){
 console.log(results);
+
+  scoreRightWrist =  results[0].pose.keypoints[10].score;
+	scoreLeftWrist =  results[0].pose.keypoints[9].score;
+	console.log("scoreRightWrist = " + scoreRightWrist + "scoreLeftWrist = " + scoreLeftWrist);
+
  leftWristX = results[0].pose.leftWrist.x;
  leftWristY = results[0].pose.leftWrist.y; 
  console.log("LeftWristX = "+leftWristX+" leftWristY = "+leftWristY);
@@ -44,23 +50,45 @@ console.log(results);
 function draw(){
 image(video, 0,0,600,500);
 
+  song1_status = song1.isPlaying();
+	song2_status = song2.isPlaying();
+
+
 fill("red");
 stroke("red")
 
-circle(leftWristX,leftWristY,20);
-circle(rightWristX,rightWristY,20);
+
+if(scoreRightWrist > 0.2)
+	{ 
+		circle(rightWristX,rightWristY,20);
+
+			song1.stop();
+
+		if(song1_status == false)
+		{
+			song2.play();
+			document.getElementById("Song_Name").innerHTML = "Playing - Obito Song"
+		}
+	}
+
+  if(scoreLeftWrist > 0.2)
+	{
+		circle(leftWristX,leftWristY,20);
+
+			song2.stop();
+
+		if(song2_status == false)
+		{
+			song1.play();
+			document.getElementById("Song_Name").innerHTML = "Playing - Beerus Song"
+		}
+	}
 }
 
-function play(){
-if(leftWristY > 0 && leftWristY <= 500){        
-song2.stop();
-song1.play();
-document.getElementById("Song_Name").innerHTML = "Beerus";
-}
-else if(rightWristY > 0 && rightWristY <= 500){ 
-song1.stop();
-song2.play();  
-document.getElementById("Song_Name").innerHTML = "Obito";  
-}
+function play()
+{
+	song.play();
+	song.setVolume(1);
+	song.rate(1);
 }
   
